cdb3f5e765f28e258bc96e0a530ed06e59dad6ac
Gus Baden <gusbaden96@gamil.com>
2020-02-19 15:20:34 -0800

    reached mvp

--- queries.sql
+++ queries.sql
@@ -2,8 +2,35 @@
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 
+-- SELECT  ProductName, CategoryName 
+-- FROM Product
+-- JOIN Category
+-- ON Product.CategoryId = Category.CategoryId;
+
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+
+-- SELECT o.id, Shipper.CompanyName 
+-- FROM "Order" AS o
+-- JOIN Shipper 
+-- ON o.ShipVia = Shipper.Id 
+-- WHERE o.OrderDate < "2012-08-09"
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
+-- SELECT ProductName, Quantity 
+-- FROM "Order" AS o
+-- JOIN "OrderDetail" AS od
+-- ON o.Id= od.OrderId
+-- JOIN "Product" as p 
+-- ON p.Id = od.ProductId 
+-- WHERE o.id = 10251
+-- ORDER by ProductName ASC
+
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+
+-- SELECT o.Id, c.CompanyName, e.LastName 
+-- FROM "Order" AS o
+-- JOIN "Customer" AS c
+-- ON o.CustomerId = c.Id
+-- JOIN "Employee" AS e
+-- ON o.EmployeeId = e.Id 
